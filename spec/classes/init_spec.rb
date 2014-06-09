2c626b612702dd299dff82284cb18be5b4269623
Evgeni Golov <evgeni@golov.de>
2014-06-09 18:13:23 +0200

    add first spec tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,29 @@
 require 'spec_helper'
 describe 'icinga2' do
+  describe 'on Debian-based systems' do
+    let (:facts) { {
+      :osfamily       => 'Debian',
+      :lsbdistid      => 'Debian',
+      :concat_basedir => '/foo',
+    } }
+    it {
+      should contain_class('icinga2')
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('icinga2') }
+      should contain_class('icinga2::service')
+      should contain_service('icinga2')
+
+      should contain_class('icinga2::package')
+      should contain_package('icinga2')
+      should contain_package('nagios-nrpe-plugin')
+
+      should contain_file('/etc/icinga2/conf.d/puppet/')
+    }
+  end
+  describe 'on other systems' do
+    it {
+      expect {
+        should compile
+      }.to raise_error(Puppet::Error, /This module only supports Debian-based systems/)
+    }
   end
 end
