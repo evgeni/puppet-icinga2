e0bb23de4f25739be66e1332816b2ff0667fb494
Evgeni Golov <sargentd@die-welt.net>
2014-06-25 08:46:33 +0200

    ensure → present should be mapped to ensure → link

--- manifests/feature.pp
+++ manifests/feature.pp
@@ -2,8 +2,15 @@ define icinga2::feature(
   $ensure = 'link'
 ) {
 
+  if $ensure == 'present' {
+    $ensure_feature = 'link'
+  }
+  else {
+    $ensure_feature = $ensure
+  }
+
   file { "/etc/icinga2/features-enabled/${name}.conf":
-    ensure  => $ensure,
+    ensure  => $ensure_feature,
     path    => "/etc/icinga2/features-enabled/${name}.conf",
     target  => "/etc/icinga2/features-available/${name}.conf",
     notify  => Service['icinga2'],
