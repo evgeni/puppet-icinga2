14d21cd886d708290bc24dfb49265b45146b2a11
Evgeni Golov <sargentd@die-welt.net>
2014-06-26 17:53:41 +0200

    clear cache after installation (just to be sure)

--- manifests/web/package.pp
+++ manifests/web/package.pp
@@ -5,6 +5,12 @@ class icinga2::web::package(
   package { $icinga2::params::web_package:
     ensure  => $ensure,
     require => [ Apt::Source['debmon_org'], Class['icinga2::ido::config'] ],
+    notify  => Exec['icinga-web-clearcache'],
+  }
+
+  exec { 'icinga-web-clearcache':
+    command     => '/usr/lib/icinga-web/bin/clearcache.sh',
+    refreshonly => true,
   }
 
 }
